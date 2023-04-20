4ac5df04fbf55db5d7147232a70b2f614eaa8554
Pooja <125560933+pooja210603@users.noreply.github.com>
2023-04-20 23:46:18 +0530

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -5,15 +5,7 @@ void main()
 int ele;
 int arr[5]={32,5,7,3,6};
 Bubblesort(arr,5);
-printf("\n Enter element to be searched \n ");
-scanf("%d",&ele);int result;
-result= BinarySearch(arr,5,ele);
-if(result==-1)
-{
-    printf("Element not found");
-}
-else
-    printf("Element found at index %d",result);
+
 }
 
 void Bubblesort(int arr[],int n)
